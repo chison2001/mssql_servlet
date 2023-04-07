fc98aba664b75e893646ce249fa8591bf9784ea7
chison2001 <sonsonsonle9a1@gmail.com>
2023-04-07 21:48:41 +0700

    first commit

---
+++ src/main/java/com/ongiuaky/config/HibernateUtil.java
@@ -0,0 +1,47 @@
+package com.ongiuaky.config;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import com.ongiuaky.entities.Product;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                settings.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=ongk;encrypt=false;");
+                settings.put(Environment.USER, "sa");
+                settings.put(Environment.PASS, "26042001");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "update");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(Product.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+}
